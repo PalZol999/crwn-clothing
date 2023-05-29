b557cf37df3346837b91ff8ff5c5925a19e1fe8c
Administrator <p.z.palasti@gmail.com>
2023-05-29 14:45:37 +0200

    authentication page compo, sign in form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import {getRedirectResult} from 'firebase/auth'
+import './authentication.styles.scss'
 
 import { 
-    auth,
+  
     signInWithGooglePopup, 
     createUserDocumentFromAuth, 
-    signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
+  } from '../../utils/firebase/firebase.utils'
+
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
 
 
-const SignIn = () => {
+const Authentication = () => {
    
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup()
@@ -19,14 +22,17 @@ const SignIn = () => {
 
    
 return (
-    <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}> Sign in with Google </button>
+    <div className='authentication-container'>
+    
+
+        <SignInForm />
         <SignUpForm />
+     
+
        
     </div>
 )
 
 }
 
-export default SignIn
+export default Authentication
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -5,6 +5,7 @@ import {
     signInWithRedirect, 
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth'
 import {
     getFirestore,
@@ -71,5 +72,11 @@ const firebaseConfig = {
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password ) return 
     return await createUserWithEmailAndPassword(auth, email, password)
+
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password ) return 
+    return await signInWithEmailAndPassword(auth, email, password)
 
 }
