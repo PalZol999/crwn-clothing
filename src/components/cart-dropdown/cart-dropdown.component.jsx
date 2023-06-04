a18b5d6e8812ed2a1d9bdb06193df1d1b4cd792c
Administrator <p.z.palasti@gmail.com>
2023-06-04 10:42:11 +0200

    adding items to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,22 @@
+import { useContext  } from 'react'
+
+import { CartContext, CartProvider } from '../../contexts/cart.context'
+
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+
 import './cart-dropdown.styles.scss'
 
 const CardDropdown = () => {
+    const {cartItems} = useContext(CartContext)
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
-            </div>
+                {cartItems.map((item) =>(
+                <CartItem key={item.id} cartItem={item} />
+                ))}
+                </div>
+                <Button> GO TO CHECKOUT</Button>
         </div>
     )
 }
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -39,7 +39,7 @@ const Navigation = ()=> {
             
         </div>
 
-               {isCartOpen && <CartDropdown />}  
+               {isCartOpen && <CartDropdown />}   
 
         </div>
  
