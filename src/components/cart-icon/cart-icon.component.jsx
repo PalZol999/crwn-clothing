06684f3f1543d8a535575095fb1d96ac4df99aec
Administrator <p.z.palasti@gmail.com>
2023-06-06 19:04:07 +0200

    hover with mouse

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,16 +1,19 @@
 
 import { useContext } from 'react'
+import CartItem from '../cart-item/cart-item.component'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import { CartContext } from '../../contexts/cart.context'
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-    const {isCartOpen, setIsCartOpen} = useContext(CartContext)
+    const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext)
     const toggleIsCartOpen= () => setIsCartOpen(!isCartOpen)
     return (
-        <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+        <div className='cart-icon-container' 
+        onMouseEnter={() => toggleIsCartOpen(true)} 
+        onMouseLeave={() => toggleIsCartOpen(false)}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartCount}</span>
 
         </div>
     )
@@ -19,3 +22,4 @@ const CartIcon = () => {
 }
 
 export default CartIcon
+
